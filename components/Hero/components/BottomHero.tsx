c1a94b1e57a39c7ab9049bca03c7a8a8d6af7576
Ivan Rodriguez <sbr.com@hotmail.com>
2023-05-29 08:56:03 -0600

    feat(add): PSUGLO-25 - Add Cypress to Next

--- components/Hero/components/BottomHero.tsx
+++ components/Hero/components/BottomHero.tsx
@@ -13,15 +13,15 @@ export const BottomHero = ({
     <div className=" d-flex align-items-center justify-content-center text-align-center">
       <Col md={8} xxxl={6}>
         <div className="text-uppercase mb-2 fs-7">
-          <span dangerouslySetInnerHTML={{ __html: title }} />
+          <span data-cy='title' dangerouslySetInnerHTML={{ __html: title }} />
         </div>
         <h2
           className="heading display-3 fw-bold text-dark mb-4"
           aria-label="No value"
         >
-          <span dangerouslySetInnerHTML={{ __html: headingText }} />
+          <span data-cy='heading' dangerouslySetInnerHTML={{ __html: headingText }} />
         </h2>
-        <a href="#" className="btn btn-primary text-white" role="button">
+        <a href="#" className="btn btn-primary text-white" role="button" data-cy='button'>
           <div className="d-flex">
             <div dangerouslySetInnerHTML={{ __html: buttonText }} />
             <Icon path={mdiArrowRight} size={1} />
@@ -30,7 +30,7 @@ export const BottomHero = ({
       </Col>
     </div>
     <div className="pt-3 pt-lg-8">
-      <Image src={img} className="card-img" rounded fluid alt="test image" />
+      <Image src={img} className="card-img" rounded fluid alt="test image" data-cy='image' />
     </div>
   </Card>
 );
