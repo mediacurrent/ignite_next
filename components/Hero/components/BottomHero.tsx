c92e8fde311b2c5c962e871c29ce7ea67137820d
Ivan Rodriguez <sbr.com@hotmail.com>
2023-06-01 15:09:37 -0600

    feat(add): TextBlock component

--- components/Hero/components/BottomHero.tsx
+++ components/Hero/components/BottomHero.tsx
@@ -13,15 +13,23 @@ export const BottomHero = ({
     <div className=" d-flex align-items-center justify-content-center text-align-center">
       <Col md={8} xxxl={6}>
         <div className="text-uppercase mb-2 fs-7">
-          <span data-cy='title' dangerouslySetInnerHTML={{ __html: title }} />
+          <span data-cy="title" dangerouslySetInnerHTML={{ __html: title }} />
         </div>
         <h2
           className="heading display-3 fw-bold text-dark mb-4"
           aria-label="No value"
         >
-          <span data-cy='heading' dangerouslySetInnerHTML={{ __html: headingText }} />
+          <span
+            data-cy="heading"
+            dangerouslySetInnerHTML={{ __html: headingText }}
+          />
         </h2>
-        <a href="#" className="btn btn-primary text-white" role="button" data-cy='button'>
+        <a
+          href="#"
+          className="btn btn-primary text-white"
+          role="button"
+          data-cy="button"
+        >
           <div className="d-flex">
             <div dangerouslySetInnerHTML={{ __html: buttonText }} />
             <Icon path={mdiArrowRight} size={1} />
@@ -30,7 +38,14 @@ export const BottomHero = ({
       </Col>
     </div>
     <div className="pt-3 pt-lg-8">
-      <Image src={img} className="card-img" rounded fluid alt="test image" data-cy='image' />
+      <Image
+        src={img}
+        className="card-img"
+        rounded
+        fluid
+        alt="test image"
+        data-cy="image"
+      />
     </div>
   </Card>
 );
--- components/Hero/components/TopHero.tsx
+++ components/Hero/components/TopHero.tsx
@@ -12,22 +12,40 @@ export const TopHero = ({
 }: HeroTypes) => (
   <Card text="dark" className="hero border-0 image_top">
     <div className="pb-3 pb-lg-8">
-      <Image data-cy='image' src={img} className="card-img" rounded fluid alt="test image" />
+      <Image
+        data-cy="image"
+        src={img}
+        className="card-img"
+        rounded
+        fluid
+        alt="test image"
+      />
     </div>
     <div className=" d-flex align-items-center justify-content-center text-align-center">
       <Col md={8} xxxl={6}>
         <div className="text-uppercase mb-2 fs-7">
           <small>
-            <span data-cy='title' dangerouslySetInnerHTML={{ __html: title }} />
+            <span data-cy="title" dangerouslySetInnerHTML={{ __html: title }} />
           </small>
         </div>
         <h2 className="heading display-3 fw-bold" aria-label="No value">
-          <span data-cy='heading' dangerouslySetInnerHTML={{ __html: headingText }} />
+          <span
+            data-cy="heading"
+            dangerouslySetInnerHTML={{ __html: headingText }}
+          />
         </h2>
         <div className="rich-text text-dark mb-3">
-          <span data-cy='paragraph' dangerouslySetInnerHTML={{ __html: paragraphText }} />
+          <span
+            data-cy="paragraph"
+            dangerouslySetInnerHTML={{ __html: paragraphText }}
+          />
         </div>
-        <a href="#" className="btn btn-primary text-white" role="button" data-cy='button'>
+        <a
+          href="#"
+          className="btn btn-primary text-white"
+          role="button"
+          data-cy="button"
+        >
           <div className="d-flex">
             <div dangerouslySetInnerHTML={{ __html: buttonText }} />
             <Icon path={mdiArrowRight} size={1} />
