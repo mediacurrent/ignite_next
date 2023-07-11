e456ae0d83bc42297f688e791335e803cf2c4985
Ivan Rodriguez <ivanrodriguez.garcia@mediacurrent.com>
2023-07-11 13:45:35 -0600

    PSUGLO-44 - Single Card

---
+++ components/SingleCard/SingleCard.cy.tsx
@@ -0,0 +1,31 @@
+import SingleCard from '.'
+
+describe('Quote', () => {
+  it('should render the TextBlock', () => {
+    cy.mount(
+      <SingleCard
+        img="https://storybook.ignite.build/images/card.webp"
+        badges={[{ text: 'New feature' }, { text: 'Announcement' }]}
+        heading="Phasellus auctor, turpis"
+        description={`This copy is optional, if nothing is entered nothing will display. Facit
+        nulla in vulputate vulputate aliquam. Commodo esse habent tation nam.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed orci
+        lacus.`}
+        buttons={[
+          {
+            text: 'Link 1'
+          },
+          {
+            text: 'Link 2',
+            primary: false
+          }
+        ]}
+      />
+    )
+    cy.get("[data-cy='image']").should('exist').should('be.visible')
+    cy.get("[data-cy='badge']").should('exist').should('be.visible')
+    cy.get("[data-cy='heading']").should('exist').should('be.visible')
+    cy.get("[data-cy='description']").should('exist').should('be.visible')
+    cy.get("[data-cy='button']").should('exist').should('be.visible')
+  })
+})
---
+++ components/SingleCard/index.tsx
@@ -0,0 +1,58 @@
+import Card from 'react-bootstrap/Card'
+import Badge from 'react-bootstrap/Badge'
+import Image from 'react-bootstrap/Image'
+import Button from '@/components/Button'
+
+import { SingleCardTypes } from '@/types/SingleCard'
+
+const SingleCard = ({
+  img,
+  badges,
+  heading,
+  description,
+  buttons
+}: SingleCardTypes) => (
+  <Card className="border-0 col-6">
+    <Image
+      data-cy="image"
+      src={img}
+      className="card-img-top"
+      alt="test image"
+    />
+
+    <div className="card-body p-0 mt-2">
+      <ul className="list-inline mb-2">
+        {badges.map(({ text }) => (
+          <li className="list-inline-item">
+            <Badge
+              className="rounded-pill p-1 px-2 fs-7 fw-semibold text-bg-primary"
+              data-cy="badge"
+            >
+              {text}
+            </Badge>
+          </li>
+        ))}
+      </ul>
+
+      <h2 className="heading card-title mb-3" data-cy="heading">
+        <a href="#" className="text-decoration-none">
+          {heading}
+        </a>
+      </h2>
+      <p
+        data-cy="description"
+        className="card-text fs-6"
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+      <ul className="list-inline mb-0">
+        {buttons.map(({ text, primary }) => (
+          <li className="list-inline-item">
+            <Button data-cy="button" buttonText={text} primary={primary} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </Card>
+)
+
+export default SingleCard
