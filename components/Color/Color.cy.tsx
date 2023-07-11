439ab29226741c74f867acf1723764911eac44e3
Ivan Rodriguez <ivanrodriguez.garcia@mediacurrent.com>
2023-07-11 14:36:56 -0600

    PSUGLO-44 - Color component

---
+++ components/Color/Color.cy.tsx
@@ -0,0 +1,51 @@
+import Color from '.'
+
+const data = [
+  {
+    bg: 'bg-primary',
+    colorText: 'text-white',
+    text: 'Primary'
+  },
+  {
+    bg: 'bg-secondary',
+    colorText: 'text-white',
+    text: 'Secondary'
+  },
+  {
+    bg: 'bg-success',
+    colorText: 'text-white',
+    text: 'Success'
+  },
+  {
+    bg: 'bg-danger',
+    colorText: 'text-white',
+    text: 'Danger'
+  },
+  {
+    bg: 'bg-warning',
+    colorText: 'text-dark',
+    text: 'Warning'
+  },
+  {
+    bg: 'bg-info',
+    colorText: 'text-dark',
+    text: 'Info'
+  },
+  {
+    bg: 'bg-light',
+    colorText: 'text-dark',
+    text: 'Light'
+  },
+  {
+    bg: 'bg-dark',
+    colorText: 'text-white',
+    text: 'Dark'
+  }
+]
+
+describe('Color', () => {
+  it('should render the TextBlock', () => {
+    cy.mount(<Color data={data} />)
+    cy.get("[data-cy='color']").should('exist').should('be.visible')
+  })
+})
---
+++ components/Color/Color.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Color from '@/components/Color'
+const meta: Meta<typeof Color> = {
+  component: Color,
+  title: 'Color',
+  tags: ['autodocs']
+}
+export default meta
+type Story = StoryObj<typeof Color>
+
+export const Primary: Story = {
+  args: {
+    data: [
+      {
+        bg: 'bg-primary',
+        colorText: 'text-white',
+        text: 'Primary'
+      },
+      {
+        bg: 'bg-secondary',
+        colorText: 'text-white',
+        text: 'Secondary'
+      },
+      {
+        bg: 'bg-success',
+        colorText: 'text-white',
+        text: 'Success'
+      },
+      {
+        bg: 'bg-danger',
+        colorText: 'text-white',
+        text: 'Danger'
+      },
+      {
+        bg: 'bg-warning',
+        colorText: 'text-dark',
+        text: 'Warning'
+      },
+      {
+        bg: 'bg-info',
+        colorText: 'text-dark',
+        text: 'Info'
+      },
+      {
+        bg: 'bg-light',
+        colorText: 'text-dark',
+        text: 'Light'
+      },
+      {
+        bg: 'bg-dark',
+        colorText: 'text-white',
+        text: 'Dark'
+      }
+    ]
+  }
+}
